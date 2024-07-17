22d677a52c8cd149b985c8e3ee91c900208e529f
sanmu <zhusen@oschina.cn>
2024-07-17 22:09:15 +0800

    feat: add header

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <div class="w-32 text-lg text-blue-400 bg-pink-500 text">sanmu</div>
-    <v-btn>
-      Button
-    </v-btn>
-    <v-card text="sanmu"></v-card>
-  </div>
+  <Header />
 </template>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row class="items-center px-12 mt-3 mb-0">
+      <v-text-field class="mr-6" label="输入关键字" variant="solo" append-inner-icon="mdi-magnify"
+        hide-details="auto"></v-text-field>
+      <v-btn variant="text" color="blue-darken-2">
+        联系我们
+      </v-btn>
+    </v-row>
+  </v-container>
+  <v-tabs selected-class="active" v-model="tab" bg-color="blue-darken-1">
+    <v-tab value="1">首页</v-tab>
+    <v-tab value="2">产品列表</v-tab>
+    <v-tab value="3">关于我们</v-tab>
+    <v-tab value="4">联系我们</v-tab>
+  </v-tabs>
+</template>
+
+<script lang="ts" setup>
+const tab = ref()
+
+</script>
+
+<style scoped>
+.active :deep .v-tab__slider {
+  bottom: 3px;
+}
+</style>
