3134ea79a41857414bdcc754ce7a05620d8a9065
sanmu <zhusen@oschina.cn>
2024-07-18 20:46:15 +0800

    feat: add footer

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
   <Header />
+  Content
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="py-4 text-white bg-gray-600">
+    <v-container>
+      <v-row>
+        <v-col cols="4" class="flex flex-col items-center ">
+          <div>
+            <b class="block mb-3">商品分类</b>
+            <p class="mb-1">男装</p>
+            <p class="mb-1">女装</p>
+            <p class="mb-1">童装</p>
+            <p class="mb-1">国风</p>
+          </div>
+        </v-col>
+        <v-col cols="4" class="flex flex-col items-center ">
+          <div>
+            <b class="block mb-3">关于我们</b>
+            <p class="mb-1">企业介绍</p>
+          </div>
+
+        </v-col>
+        <v-col cols="4" class="flex flex-col items-center ">
+          <div>
+            <b class="block mb-3">联系我们</b>
+            <p class="mb-1">Email: sanmu@emial.com</p>
+            <p class="mb-1">Phone: 182xxxxxxxx</p>
+            <p class="mb-1">Wechat: sanmu1598</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
